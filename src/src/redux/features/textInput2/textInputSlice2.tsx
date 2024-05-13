e9fe9ebdb49df378b63cd5e337e7a6aa267e9c5a
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2024-05-13 18:42:32 +0300

    Created slice for textInput2

---
+++ src/src/redux/features/textInput2/textInputSlice2.tsx
@@ -0,0 +1,29 @@
+import {RootState} from '@redux';
+import type {PayloadAction} from '@reduxjs/toolkit';
+import {createSlice} from '@reduxjs/toolkit';
+
+interface TextInputState {
+  value: string;
+}
+
+const initialState: TextInputState = {
+  value: '',
+};
+
+const textInputSlice = createSlice({
+  name: 'textInput',
+  initialState,
+  reducers: {
+    setTextInput1: (state, action: PayloadAction<string>) => {
+      state.value = action.payload;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+const {setTextInput1} = textInputSlice.actions;
+const textInputSelector1 = (state: RootState) => state.textInput1.value;
+
+export {textInputSelector1, setTextInput1, textInputSlice};
+export type {TextInputState};
+export default textInputSlice.reducer;

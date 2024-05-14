27c345e6f2a2d7725b002770366b98e16adb91ec
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2024-05-14 18:43:06 +0300

    Data persisted carefully

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import {store} from '@redux';
 import React from 'react';
 import {Provider} from 'react-redux';
 import MainApp from './src/App';
+import {persistor} from '@redux';
+import {PersistGate} from 'redux-persist/integration/react';
 
 function App() {
   return (
     <Provider store={store}>
-      <MainApp />
+      <PersistGate loading={null} persistor={persistor}>
+        <MainApp />
+      </PersistGate>
     </Provider>
   );
 }

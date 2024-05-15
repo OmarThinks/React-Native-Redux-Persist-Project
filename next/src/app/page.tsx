5e08e7633bd3da2511df2da1834840e9164dceef
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2024-05-15 17:58:23 +0300

    page: provided Redux with presistor

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,11 +1,18 @@
 "use client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "@redux";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <p>Hey</p>
-      </div>
-    </main>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <div>
+            <p>Hey</p>
+          </div>
+        </main>
+      </PersistGate>
+    </Provider>
   );
 }
